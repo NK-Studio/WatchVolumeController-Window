e49517265488735facc6f990244779a88b6fd758
NK Studio <53283888+NK-Studio@users.noreply.github.com>
2022-12-06 02:24:32 +0900

    Init

---
+++ test2WatchController/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace test2WatchController
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.WatchVolumeController = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // WatchVolumeController
+            // 
+            this.WatchVolumeController.Icon = ((System.Drawing.Icon)(resources.GetObject("WatchVolumeController.Icon")));
+            this.WatchVolumeController.Text = "WatchVolumeController";
+            this.WatchVolumeController.Visible = true;
+            this.WatchVolumeController.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "WatchVolumeController";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.NotifyIcon WatchVolumeController;
+
+        #endregion
+    }
+}
---
+++ test2WatchController/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test2WatchController
+{
+    public partial class Form1 : Form
+    {
+        private UdpClient client;
+        public Form1()
+        {
+            WindowState = FormWindowState.Minimized;
+            ShowInTaskbar = false;
+            InitializeComponent();
+
+            client = new UdpClient(9090);
+            
+            StartListening();
+        }
+
+        private void StartListening()
+        {
+            client.BeginReceive(ReceivedData, client);
+        }
+        
+        private void ReceivedData(IAsyncResult asyncResult)
+        {
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse("172.30.1.97"), 9090);
+            byte[] received = client.EndReceive(asyncResult, ref ipEndPoint);
+            
+            string index = Encoding.UTF8.GetString(received, 0, received.Length);
+            
+            switch (index)
+            {
+                case "0":
+                    ExecuteCmd("nircmd.exe changesysvolume 2000");
+                    break;
+                case "1":
+                    ExecuteCmd("nircmd.exe changesysvolume -2000");
+                    break;
+            }
+
+            StartListening();
+        }
+
+        /// <summary>
+        /// textKey : 실행할 명령어 
+        /// </summary>
+        /// <param name="textKey"></param>
+        /// <returns></returns>
+        private static void ExecuteCmd(string textKey)
+        {
+            ProcessStartInfo pri = new ProcessStartInfo();
+            Process pro = new Process();
+
+            pri.FileName = @"cmd.exe";
+            pri.CreateNoWindow = true;
+            pri.UseShellExecute = false;
+
+            pri.RedirectStandardInput = true; //표준 출력을 리다이렉트
+            pri.RedirectStandardOutput = true;
+            pri.RedirectStandardError = true;
+
+            pro.StartInfo = pri;
+            pro.Start(); //어플리케이션 실
+
+            pro.StandardInput.Write(textKey + Environment.NewLine);
+            pro.StandardInput.Close();
+
+            pro.WaitForExit();
+            pro.Close();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+            WatchVolumeController.Visible = false;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                WatchVolumeController.Visible = true;
+            }
+        }
+        
+    }
+}
